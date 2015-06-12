29739f6fb1a507ef5ec96959d97cb6ab47846b62
Jiahao Li <isundaylee.reg@gmail.com>
2015-06-12 01:05:44 -0700

    Add verbose option.

--- youtube-mp3.rb
+++ youtube-mp3.rb
@@ -10,9 +10,10 @@ def usage
   exit
 end
 
-def run(title, command)
-  command += ' 2>&1 >/dev/null'
+def run(title, command, verbose)
+  command += ' 2>&1 >/dev/null' unless verbose
   puts title + " ..."
+  puts command if verbose
   `#{command}`
 end
 
@@ -28,6 +29,7 @@ opts = Trollop::options do
 
   opt :import, "Import into iTunes. "
   opt :output, "Output file. Defaults to song.mp3.", type: String
+  opt :verbose, "Output verbose log. "
 end
 
 if ARGV.size < 1
@@ -41,14 +43,14 @@ output_m4a = File.join(File.dirname(output), "#{File.basename(output, '.*')}.m4a
 output_mp3 = File.join(File.dirname(output), "#{File.basename(output, '.*')}.mp3")
 
 FileUtils.rm_f(output_mp3)
-run('Downloading', "youtube-dl -x '#{url}' -o #{output}")
+run('Downloading', "youtube-dl -x '#{url}' -o #{output}", opts[:verbose])
 
 if !File.exists?(output_mp3)
-  run('Converting', "ffmpeg -i #{output_m4a} -q:a 0 -f mp3 #{output_mp3}")
+  run('Converting', "ffmpeg -i #{output_m4a} -q:a 0 -f mp3 #{output_mp3}", opts[:verbose])
   FileUtils.rm_f(output_m4a)
 end
 
-run('Tagging', 'echo')
+run('Tagging', 'echo', opts[:verbose])
 info = {
   album: 'YouTube',
   title: `youtube-dl -e '#{url}'`.strip,
@@ -59,5 +61,5 @@ info = {
 ID3Writer.write_id3(output_mp3, info)
 
 if opts[:import]
-  run('Importing', "mv #{output_mp3} ~/Music/iTunes/iTunes\\ Media/Automatically\\ Add\\ to\\ iTunes.localized/")
+  run('Importing', "mv #{output_mp3} ~/Music/iTunes/iTunes\\ Media/Automatically\\ Add\\ to\\ iTunes.localized/", opts[:verbose])
 end
